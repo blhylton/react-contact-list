286186ad72abb68519dbe5d7727f8062901c2b7c
Barry Hylton <blhylton@blhylton.com>
2017-06-28 19:46:40 -0400

    Looped out Contacts within ListContacts
    Intermediary step to make sure props are passing as expected.

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -4,7 +4,9 @@ class ListContacts extends Component{
     render() {
         return (
             <ol className="contact-list">
-                
+                {this.props.contacts.map(
+                    (contact) => <li key={contact.id}>{contact.name}</li>
+                )}
             </ol>
         );
     }
